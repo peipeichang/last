f30ab85c79bede8d908d948512b4a220c3dd36a4
peipeichang <62254304+peipeichang@users.noreply.github.com>
2021-05-24 01:59:22 +0800

    first

---
+++ src/.temp/config.js
@@ -0,0 +1,8 @@
+export default {
+  "trailingSlash": true,
+  "pathPrefix": "/test",
+  "titleTemplate": "%s - Gridsome",
+  "siteUrl": "https://peipeichang.github.io",
+  "version": "0.7.23",
+  "catchLinks": true
+}
